b629fd2017910b28bbf674ba66226a8f73ee30a7
dderoose <dennis.deroose.w0495@student.hogent.be>
2017-11-25 15:55:07 +0100

    added testen

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -57,7 +57,7 @@ export class TopicComponent implements OnInit {
     console.log("U");
     console.log(this.selectedTopic.name);
     console.log("l");
-    console.log(this.topics.toString);
+    console.log(this.topics.pop);
     topic.name = 'klm';
     console.log(topic.name);
     this.topics.push(topic);
