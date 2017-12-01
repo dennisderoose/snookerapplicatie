7823920fe95e5b18da73b314d413088147804625
dderoose <dennis.deroose.w0495@student.hogent.be>
2017-12-01 09:58:18 +0100

    Try to get from login to register

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
+//import { AuthenticationService } from '../authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user: FormGroup;
+  public errorMsg: string;
 
-  constructor() { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.user = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    /*this.authService.login(this.user.value.username, this.user.value.password).subscribe(val => {
+      if (val) {
+        console.log(val);
+        if (this.authService.redirectUrl) {
+          this.router.navigateByUrl(this.authService.redirectUrl);
+          this.authService.redirectUrl = undefined;
+        } else {
+          this.router.navigate(['/recipe/list']);
+        }
+      }
+    }, err => this.errorMsg = err.json().message);*/
+  }
 }
