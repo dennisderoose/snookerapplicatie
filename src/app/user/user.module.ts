e9197c892a2a5bab908af1711ac534a99063ce65
dderoose <dennis.deroose.w0495@student.hogent.be>
2017-12-01 16:31:53 +0100

    Add link

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,7 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 //import { AuthenticationService } from './authentication.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -27,7 +28,8 @@ const routes = [
     UserComponent,
     LoginComponent,
     RegisterComponent,
-    LogoutComponent
+    LogoutComponent,
+    FormsModule
   ],
   /*providers: [
     AuthenticationService,
