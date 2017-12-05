f2b63fb44899479757698e39685d55c18e58d49c
dderoose <dennis.deroose.w0495@student.hogent.be>
2017-12-05 10:01:43 +0100

    test

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -25,7 +25,6 @@ export class RegisterComponent implements OnInit {
   public usertje: FormGroup;
 
   get passwordControl(): FormControl {
-    console.log(this.usertje.get('passwordGroup').get('password'));
     return <FormControl>this.usertje.get('passwordGroup').get('password');
   }
 
