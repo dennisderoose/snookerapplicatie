e791b0193c1b07d96a597b424f59c5902fbfabf6
dderoose <dennis.deroose.w0495@student.hogent.be>
2017-12-05 10:30:11 +0100

    test

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -21,6 +21,6 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
-    this.router.navigate(['./topic']);
+    this.router.navigate(['../topic']);
   }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import { HttpModule} from '@angular/http';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
+import { TopicComponent } from '../topic/topic.component';
 import { UserComponent } from 'app/user/user.component';
 import { UserService } from 'app/user/user.service';
 
@@ -16,7 +17,8 @@ const routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'user', component: UserComponent }
+  { path: 'user', component: UserComponent },
+  { path: 'topic', component: TopicComponent }
 ];
 
 @NgModule({
