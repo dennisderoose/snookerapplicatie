4eed79412dbc5cf766dd1cbd81656d2c4025dc08
dennisderoose <dennisderoose@hotmail.com>
2017-12-30 13:17:08 +0100

    database aanpassen

--- models/Topic.js
+++ models/Topic.js
@@ -1,9 +1,17 @@
 let mongoose = require('mongoose');
 
 let TopicSchema = new mongoose.Schema({
+    id: String,
     name: String,
     vraag: String,
-    opmerkingen: [{type: mongoose.Schema.Types.ObjectId, ref: 'Opmerking'}]
+    opmerkingen: [{
+        name: String,
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Opmerking'
+        }
+    }],
+    user: String
 });
 
 mongoose.model('Topic', TopicSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 var router = express.Router();
 let mongoose = require("mongoose");
@@ -21,9 +22,12 @@ router.get("/webapptaak/topics/", function(req, res, next) {
 });
 
 router.post("/webapptaak/topics/", auth, function(req, res, next) {
+  console.log("h");
   let topic = new Topic({
     name: req.body.name,
-    vraag: req.body.vraag
+    vraag: req.body.vraag,
+    opmerkingen: req.body.opmerkingen,
+    user: req.body.user
   });
   topic.save(function(err, post) {
     if (err) {
@@ -32,6 +36,65 @@ router.post("/webapptaak/topics/", auth, function(req, res, next) {
     res.json(topic);
   });
 });
+
+router.put("/webapptaak/topics/:id", auth, function(req, res) {
+  console.log("k");
+  console.log(req.body.opmerkingen);
+/*
+  let opm = new Opmerking(req.body);
+  console.log(opm);
+  opm.save(function(err, opmerking) {
+    if (err) return next(err);
+
+    req.Topic.opmerkingen.push(opmerking);
+    req.Topic.save(function(err, rec) {
+      if (err) return next(err);
+      res.json(opmerking);
+    })
+  });*/
+
+Topic.findById(req.params.id, function(err, Topic) {
+  if(err) {
+    res.send(err);
+  }
+  Topic.opmerkingen = req.body.opmerkingen;
+
+  Topic.save(function(err) {
+    if (err)
+        res.send(err);
+
+    res.json({ message: 'Topic updated!' });
+  });
+});
+
+/*
+  Topic.findOneAndUpdate({
+      _id: req.params.id
+  },{$set: {
+    opmerkingen: req.body.opmerkingen
+  }}, {upsert: true}, function(err, Topic) {
+    if(err) {
+      console.log(Topic);
+      res.status(500).send(err)
+    } else {
+      console.log(Topic);
+      console.log("gelukt");
+      res.status(204).send()
+    }
+  });
+  /*let topic = new Topic({
+    name: req.body.name,
+    vraag: req.body.vraag,
+    opmerkingen: req.body.opmerkingen
+  });
+  topic.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    res.json(topic);
+  });*/
+});
+
 
 router.param("topic", function(req, res, next, id) {
   let query = Topic.findById(id);
@@ -47,17 +110,19 @@ router.param("topic", function(req, res, next, id) {
   });
 });
 
+
+
 router.get("/webapptaak/topics/:topic", function(req, res) {
   req.topic.populate("opmerkingen", function(err, rec) {
     if (err) return next(err);
     res.json(rec);
   });
 });
-
+/*
 router.post("/webapptaak/topic/:topic/opmerkingen", function(req, res, next) {
-  let ing = new Opmerking(req.body);
-  console.log(ing);
-  ing.save(function(err, opmerking) {
+  let opm = new Opmerking(req.body);
+  console.log(opm);
+  opm.save(function(err, opmerking) {
     if (err) return next(err);
 
     req.topic.opmerkingen.push(opmerking);
@@ -66,6 +131,6 @@ router.post("/webapptaak/topic/:topic/opmerkingen", function(req, res, next) {
       res.json(opmerking);
     });
   });
-});
+});*/
 
 module.exports = router;
