83c44c12276621e3031a345e27750e2807d3ca6f
dderoose <dennis.deroose.w0495@student.hogent.be>
2018-03-29 18:11:11 +0200

    verwijderen break

--- src/app/break/break/break.component.ts
+++ src/app/break/break/break.component.ts
@@ -121,6 +121,6 @@ export class BreakComponent implements OnInit {
       this._snookerDataService.removeBreak(brak._id).subscribe(item =>
         this._breaks = this._breaks.filter(val => item.id !== val.id)
       );
-      this._router.navigate(['break']);
+      this._router.navigate(['break'], { queryParams: { user: this.user} });
     }
 }
